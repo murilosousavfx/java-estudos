9e529cd11acffbb287d84508dfb6e8db244acf4a
murilo.sousa <murilovfx10@gmail.com>
2026-03-10 11:04:23 -0300

    Criando um catálogo de filmes

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class subtracao{
+    public static void main(String[] args){
+        System.out.println(10 - 5 );
+    }
+}
+    
--- src/Perfil.java
+++
@@ -1,7 +0,0 @@
-public class Perfil{
-    public static void main(String[] args){
-        System.out.println("Olá, Murilo!");
-        System.out.println("Tudo bem?");
-    }
-}
-
